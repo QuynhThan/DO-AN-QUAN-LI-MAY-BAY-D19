c6f745ad4034efd007b7722da3930f0b531338ce
QuynhThan <85308155+QuynhThan@users.noreply.github.com>
2021-07-09 22:51:57 +0700

    UPLOADS FILE

---
+++ QUAN_LI_MAY_BAY.cpp
@@ -0,0 +1,11 @@
+
+#include "menu.h"
+
+int main() {
+	menu();
+	/*DATE_TIME d1, d2;
+	d1.phut = 50; d1.gio = 23; d1.ngay = 1; d1.thang = 1, d1.nam = 2021;
+	d2.phut = 55; d2.gio = 23; d2.ngay = 1; d2.thang = 1, d2.nam = 2021;
+	cout << ktra_tgian(d1, d2);*/
+	return 0;
+}
